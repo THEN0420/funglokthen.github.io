e72fa39f443fea1623b24fa92d928e4cec1aca13
Fung Lok Then <gem.ice.fantasy@gmail.com>
2021-07-04 20:22:42 +0800

    version 1

---
+++ order.js
@@ -0,0 +1,11 @@
+
+function calcOrder(){
+var quantity=document.getElementById("quantity").value;
+var price=document.getElementById("price").value;
+var total=quantity * price;
+document.getElementById("totalCost").innerHTML = total
+
+}
+
+
+
